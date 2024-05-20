c6932b31ec6d2a79d4bbd7b0889782ed673cde8b
Rosaury Mariana Hernandez <98234894+Rosauher18@users.noreply.github.com>
2024-05-20 17:29:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Longitud en metros: ");
+            double metro = Convert.ToDouble(Console.ReadLine());
+
+            double pie = metro * 3.28084;
+
+            Console.WriteLine("{0} metros son {1} pies.", metro, pie);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
